a478b05114a2412690c35f78d034f2d913ec9e60
amineallali <amineallali9.com>
2024-05-02 13:55:51 +0100

    modified:   includes/sidebar_admin.php

--- includes/sidebar_admin.php
+++ includes/sidebar_admin.php
@@ -9,8 +9,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.5.1/css/all.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.min.js"></script>
-  <link rel="stylesheet" href="style_admin.css">
-  <script src="../../static/js/chart.js"></script>
+  <link rel="stylesheet" href="../views/admin/style_admin.css">
 </head>
 
 <body id="page-top">
